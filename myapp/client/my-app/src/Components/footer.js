ff076e19df00649e9d6d2df9369d4d39596b8a78
lakshaycode22 <lakshaycode22@gmail.com>
2021-10-08 01:34:37 +0530

    Home page

---
+++ myapp/client/my-app/src/Components/footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+ 
+class Footer extends Component {
+    render() {
+        return (
+            <footer id="footer">
+    <div class="container">
+      <div class="credits">
+        
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer>
+)
+    }
+}
+export default Footer;
