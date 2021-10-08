478ceedce742fa91162dc6b1c44513061c8d0d98
lakshaycode22 <lakshaycode22@gmail.com>
2021-10-08 15:21:08 +0530

    hero

--- myapp/client/src/Components/footer.js
+++ myapp/client/src/Components/footer.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
- 
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 class Footer extends Component {
-    render() {
-        return (
-            <footer id="footer">
-    <div class="container">
-      <div class="credits">
-        
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer>
-)
-    }
+  render() {
+    return (
+      <footer id="footer">
+        <div class="container">
+          <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 export default Footer;
