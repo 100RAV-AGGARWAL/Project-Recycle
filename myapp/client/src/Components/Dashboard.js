081ca15c2a58468df33404bab4aaae2f898e0adb
lakshaycode22 <lakshaycode22@gmail.com>
2021-10-09 12:45:32 +0530

    dashboard

--- myapp/client/src/Components/Dashboard.js
+++ myapp/client/src/Components/Dashboard.js
@@ -1,10 +1,22 @@
 import React from "react";
 const Dashboard = (props) => {
   return (
-    <>
-       <h2>Private Dashboard</h2>
-        Email: {props.payload.identifier}
-    </>
+    <div className="dashboard d-flex align-items-center">
+      <header id="header" class="fixed-top">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+          <h1 class="logo me-auto me-lg-0">
+            <a href="index.html">Private Dashboard</a>
+          </h1>
+
+          <a class="email">Email: {props.payload.identifier}</a>
+        </div>
+      </header>
+      <div className="login-success">
+        <div className="login-text">Login Successful</div>
+
+        <a className="btn code-btn">Generate Code</a>
+      </div>
+    </div>
   );
 };
 export default Dashboard;
